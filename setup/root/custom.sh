aa81adef515d1cc1f14f906beb4331e9001014c4
binhex <megalith01@gmail.com>
2017-04-16 20:41:39 +0100

    put in curl retry and backoff

--- setup/root/custom.sh
+++ setup/root/custom.sh
@@ -8,10 +8,10 @@ app_name="Medusa"
 install_name="medusa"
 
 # find latest release tag from github
-release_tag=$(curl -s "https://github.com/${repo_name}/${app_name}/releases" | grep -P -o -m 1 "(?<=/${repo_name}/${app_name}/releases/tag/)[^\"]+")
+release_tag=$(curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 -s "https://github.com/${repo_name}/${app_name}/releases" | grep -P -o -m 1 "(?<=/${repo_name}/${app_name}/releases/tag/)[^\"]+")
 
 # download install zip file
-curl -o "/tmp/${app_name}-release.zip" -L "https://github.com/${repo_name}/${app_name}/archive/${release_tag}.zip"
+curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 -o "/tmp/${app_name}-release.zip" -L "https://github.com/${repo_name}/${app_name}/archive/${release_tag}.zip"
 
 # unzip to /tmp
 unzip "/tmp/${app_name}-release.zip" -d /tmp

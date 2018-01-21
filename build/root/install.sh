fed2ee3ceea342b2003fc56bfcb5055b5f77da0a
binhex <megalith01@gmail.com>
2018-01-21 20:41:39 +0000

    also needed libx264

--- build/root/install.sh
+++ build/root/install.sh
@@ -19,7 +19,7 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
 ####
 
 # define pacman packages
-pacman_packages="python2 python2-cheetah python2-pyopenssl libvpx"
+pacman_packages="python2 python2-cheetah python2-pyopenssl libx264 libvpx"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
